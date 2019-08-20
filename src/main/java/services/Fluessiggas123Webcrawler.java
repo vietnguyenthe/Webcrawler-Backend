91c6ffeddf92d90f462b993cd32b7148f0b2ba34
kazim <kazim.guevenc@gmail.com>
2019-08-20 13:27:38 +0200

    Webcrawler für Flüssiggas123_ alle drei behälter

--- src/main/java/services/Fluessiggas123Webcrawler.java
+++ src/main/java/services/Fluessiggas123Webcrawler.java
@@ -8,13 +8,20 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Random;
+
 
 @Service
 public class Fluessiggas123Webcrawler {
 
     // Voraussetzungen setzen
     String url = "https://www.123-fluessiggas.de/";
-    String plz = "50321";
+    String [] plzListe = {"50129", "50189", "50354", "51371", "51545", "51645",
+        "52072", "52222", "52372", "53050", "53173", "53359"  };
+    Random r = new Random();
+//    String plz = "50321";
+
     String fuellstand = "30";
 
     String behaelterKleinId = "behaelter1";
@@ -29,59 +36,50 @@ public class Fluessiggas123Webcrawler {
     String bestellmenge = "";
     String preisProLiterNetto = "";
 
-    // Starte Chrome und Startseite
-//    WebDriver driver = new ChromeDriver();
+    WebDriver driver = starteDriver();
 
     // JavascriptExector hilft beim Runterscrollen der Seite, weil ansonsten Silenium die unteren Elemente nicht anklicken kann
-    JavascriptExecutor executor = (JavascriptExecutor)this.starteDriver();
+    JavascriptExecutor executor = (JavascriptExecutor)driver;
+
 
     public WebDriver starteDriver (){
+        // Starte Chrome und Startseite
         System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src\\main\\resources\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        driver.get(url);
         return driver;
     }
 
-    WebDriver driver = this.starteDriver();
-
-
-
-/*    public Fluessiggas123Webcrawler(WebDriver driver) {
-        this.url = url;
-        this.driver = driver;
-        this.executor = executor;
-
-        // Starte Chrome und Startseite
-        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src\\main\\resources\\chromedriver.exe");
-
-        driver.get(url);
-        // JavascriptExector hilft beim Runterscrollen der Seite, weil ansonsten Silenium die unteren Elemente nicht anklicken kann
-        executor = (JavascriptExecutor)driver;
-    }*/
 
     public void start(){
-        // Starte Chrome und Startseite
-//        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src\\main\\resources\\chromedriver.exe");
-  /*      ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--headless");
-        */
-
-        driver.get(url);
-        // JavascriptExector hilft beim Runterscrollen der Seite, weil ansonsten Silenium die unteren Elemente nicht anklicken kann
-//        JavascriptExecutor executor = (JavascriptExecutor)driver;
-//        executor = (JavascriptExecutor)driver;
-
+        // Einmal alles eingeben und mit der kleinen Behältergröße beginnen
         Fluessiggas123Webcrawler fluessiggas123Webcrawler = new Fluessiggas123Webcrawler( );
         fluessiggas123Webcrawler.behaelterGroesseAuswaehlen(behaelterKleinId);
         fluessiggas123Webcrawler.plzAuswaehlen();
-        fluessiggas123Webcrawler.fuellstandBestellmengeAuswaehlen();
+        fluessiggas123Webcrawler.fuellstandAuswaehlen();
+        fluessiggas123Webcrawler.bestellmengeAuswaehlen();
+        fluessiggas123Webcrawler.liefermengenAusgabe();
+        fluessiggas123Webcrawler.preisAusgabe();
+
+
+
+        // Zweites mal durchgehen aber diesmal die mittlere Behältergröße auswählen
+        fluessiggas123Webcrawler.behaelterGroesseAuswaehlen(behaelterMittelId);
+        fluessiggas123Webcrawler.bestellmengeAuswaehlen();
+        fluessiggas123Webcrawler.liefermengenAusgabe();
+        fluessiggas123Webcrawler.preisAusgabe();
+
+        // Drittes mal durchgehen aber diesmal die große Behältergröße auswählen
+        fluessiggas123Webcrawler.behaelterGroesseAuswaehlen(behaelterGroßId);
+        fluessiggas123Webcrawler.bestellmengeAuswaehlen();
         fluessiggas123Webcrawler.liefermengenAusgabe();
         fluessiggas123Webcrawler.preisAusgabe();
     }
 
 
-    public void behaelterGroesseAuswaehlen (String behaelterGroessenId){
+    public void behaelterGroesseAuswaehlen (String behaelterGroessenEingabe){
         // Finde das Element für den Kleinenbehaelter und wähle es aus
-        WebElement behaelter = driver.findElement(By.id(behaelterGroessenId));
+        WebElement behaelter = driver.findElement(By.id(behaelterGroessenEingabe));
         executor.executeScript("arguments[0].click();", behaelter);
     }
 
@@ -89,15 +87,19 @@ public class Fluessiggas123Webcrawler {
         // Finde Input Feld für Postleitzahl und füge die PLZ ein
         WebElement plzEingabe = driver.findElement(By.id(plzEingabeId));
         executor.executeScript("arguments[0].click()", plzEingabe);
-        plzEingabe.sendKeys(plz);
+        String plzZufall = plzListe[r.nextInt(plzListe.length)];
+        plzEingabe.sendKeys(plzZufall);
+        System.out.println("Zufällig ausgewählte PLZ: " + plzZufall);
     }
 
-    public void fuellstandBestellmengeAuswaehlen (){
+    public void fuellstandAuswaehlen (){
         // FInde Input Feld für den Füllstand und schreibe immer 30 rein egal für welche Behältergröße
         WebElement fuellstandEingabe = driver.findElement(By.id(fuellstandEingabeId));
         executor.executeScript("arguments[0].click()", fuellstandEingabe);
         fuellstandEingabe.sendKeys(fuellstand);
+    }
 
+    public void bestellmengeAuswaehlen(){
         // Finde das Element für die Bestellmenge und wähle immer Vollieferung aus egal für welche Behältergröße
         WebElement bestellmengeEingabe = driver.findElement(By.id(bestellmengeId));
         executor.executeScript("arguments[0].click();", bestellmengeEingabe);
