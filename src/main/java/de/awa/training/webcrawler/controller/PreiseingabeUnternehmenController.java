8ed0d03bcaae1471baa9e9dc70bae8acc7f0342c
viet_nguyenthe <the_viet_nguyen@yahoo.de>
2019-08-26 15:46:20 +0200

    Repo für Preiseingaben bauen (ungoing)

--- src/main/java/de/awa/training/webcrawler/controller/PreiseingabeUnternehmenController.java
+++ src/main/java/de/awa/training/webcrawler/controller/PreiseingabeUnternehmenController.java
@@ -1,5 +1,37 @@
 package de.awa.training.webcrawler.controller;
 
+import de.awa.training.webcrawler.entity.PreiseingabeUnternehmenEntity;
+import de.awa.training.webcrawler.model.PreiseingabeUnternehmen;
+import de.awa.training.webcrawler.repository.PreiseingabeUnternehmenRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 public class PreiseingabeUnternehmenController {
 
+    // Unternehmensanfrage aus dem Frontend entgegennehmen und in der Datenbank speichern
+    @PostMapping("/kontaktUnternehmen")
+    public void unternehmensAnfrage (@RequestBody KontaktAnfrage kontaktAnfrage){
+
+        // Entity Kontaktanfrage erstellen und dann die erhaltenen Daten mit dem Setter und getter festlegen + speichern
+        KontaktanfrageEntity kontaktanfrageEntity = new KontaktanfrageEntity();
+
+        kontaktanfrageEntity.setBetreff(kontaktAnfrage.getBetreff());
+        kontaktanfrageEntity.setFirmennamen(kontaktAnfrage.getFirmennamen());
+        kontaktanfrageEntity.setFirmenadresse(kontaktAnfrage.getFirmenadresse());
+        kontaktanfrageEntity.setOrt(kontaktAnfrage.getOrt());
+        kontaktanfrageEntity.setPlz(kontaktAnfrage.getPlz());
+        kontaktanfrageEntity.setKontaktperson(kontaktAnfrage.getKontaktperson());
+        kontaktanfrageEntity.setEmailAdresse(kontaktAnfrage.getEmailAdresse());
+        kontaktanfrageEntity.setNachricht(kontaktAnfrage.getNachricht());
+
+        kontaktanfrageRepository.save(kontaktanfrageEntity);
+    }
+
+    @PostMapping("/preiseingabe")
+    public void preiseingabe(@RequestBody PreiseingabeUnternehmen preiseingabeUnternehmen){
+
+        PreiseingabeUnternehmenEntity preiseingabeUnternehmenEntity = new PreiseingabeUnternehmenEntity();
+
+        PreiseingabeUnternehmenRepository.save(preiseingabeUnternehmenEntity);
+    }
 }
