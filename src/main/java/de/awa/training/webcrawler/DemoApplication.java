89346ce7d218b44e3671cf04a49cce5130896327
Felix Weyer <f.weyer88@googlemail.com>
2019-08-27 12:18:00 +0200

    Unternehmensnamen

--- src/main/java/de/awa/training/webcrawler/DemoApplication.java
+++ src/main/java/de/awa/training/webcrawler/DemoApplication.java
@@ -50,9 +50,9 @@ public class DemoApplication {
     private void init() {
         String[]postleitzahlen = {"51570","52152","53804", "54290","55543","56072","57539","58119"};
         String [][]unternehmen = {
-                {"Propan Rheingas GmbH & Co. KG","Fischenicher Straße 23","50321","Brühl","pfiffiggas","pfiffiggas"},
-                {"DFG Deutsche Flüssiggas GmbH","Stau 169","262122","Oldenburg","meinfluessiggas","meinfluessiggas"},
-                {"TEGA - Technische Gase und Gasetechnik GmbH", "Blumenstraße 70","99092 ","Erfurt","fluessiggas123", "fluessiggas123"},
+                {"Pfiffigas","Fischenicher Straße 23","50321","Brühl","pfiffiggas","pfiffiggas"},
+                {"Meinflüssiggas","Stau 169","262122","Oldenburg","meinfluessiggas","meinfluessiggas"},
+                {"123Flüssiggas", "Blumenstraße 70","99092 ","Erfurt","fluessiggas123", "fluessiggas123"},
                 {"Beispielunternehmen", "ABC str.90","50000", "Köln","bespiel","beispiel"},
                 {"Testunternehmen", "DEF str.100","60000", "Köln","test","test"},
         };
