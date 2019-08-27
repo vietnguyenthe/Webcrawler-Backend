175656c3b7f99a3ffb48e45804f2467fbf1e678d
kazim <kazim.guevenc@gmail.com>
2019-08-27 16:34:48 +0200

    2708 Preiseingabe Unternehmen in Datenbank gespeichert und richtige UnId + PlZID eingefügt Fehlt noch: wenn plz nicht vorhanden dann erst hinzufügen dann id eintragen

--- src/main/java/de/awa/training/webcrawler/controller/PreiseingabeUnternehmenController.java
+++ src/main/java/de/awa/training/webcrawler/controller/PreiseingabeUnternehmenController.java
@@ -1,13 +1,16 @@
 package de.awa.training.webcrawler.controller;
 
+import de.awa.training.webcrawler.entity.PostleitzahlenEntity;
 import de.awa.training.webcrawler.entity.PreiseingabeUnternehmenEntity;
 import de.awa.training.webcrawler.entity.UnternehmenEntity;
 import de.awa.training.webcrawler.model.PreiseingabeUnternehmen;
+import de.awa.training.webcrawler.repository.PLZRepository;
 import de.awa.training.webcrawler.repository.PreiseingabeUnternehmenRepository;
 import de.awa.training.webcrawler.repository.UnternehemensRepository;
 import de.awa.training.webcrawler.services.AnfragenService;
 import net.bytebuddy.asm.Advice;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +19,7 @@ import javax.persistence.GeneratedValue;
 import java.sql.Date;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.Arrays;
 
 @RestController
 public class PreiseingabeUnternehmenController {
@@ -29,17 +33,79 @@ public class PreiseingabeUnternehmenController {
     @Autowired
     UnternehemensRepository unternehemensRepository;
 
+    @Autowired
+    PLZRepository plzRepository;
+
+
+    @CrossOrigin("http://localhost:3000")
     @PostMapping("/preiseingabe")
     public void preiseingabe(@RequestBody PreiseingabeUnternehmen preiseingabeUnternehmen) {
 
+        // Voraussetzungen
         PreiseingabeUnternehmenEntity preiseingabeUnternehmenEntity = new PreiseingabeUnternehmenEntity();
+
+        // Dateneingabe gehört zu welchem Unternehmen? Nach UN_Id suchen und festlegen anhand des Passwortes
+        String f = preiseingabeUnternehmen.getKennwort();
+        int m = f.hashCode();
+
+        for(UnternehmenEntity k : unternehemensRepository.findAll()){
+            if( k.getPasswort().equals(m)){
+                preiseingabeUnternehmenEntity.setUnternehmenId(k.getId());
+
+                // Danach Json Daten vom Frontend in PreisEingabeUnternehmenEntity(Datenbank) speichern
+                preiseingabeUnternehmenEntity.setDatum(new Date(new java.util.Date().getTime()));
+                preiseingabeUnternehmenEntity.setPreis2700Liter(preiseingabeUnternehmen.getPreis2700Liter());
+                preiseingabeUnternehmenEntity.setPreis4850Liter(preiseingabeUnternehmen.getPreis4850Liter());
+                preiseingabeUnternehmenEntity.setPreis6400Liter(preiseingabeUnternehmen.getPreis6400Liter());
+
+                // zusätzlich noch die restlichen fehlenden Attribute für PreisEingabeUnternehmentEntity festlegen wie PlzId
+                boolean plzEnthalten = false;
+                int plz = 0;
+                for(PostleitzahlenEntity i: plzRepository.findAll()){
+                    if(preiseingabeUnternehmen.getPostleitzahl().equals(i.getPlz())){
+                       plzEnthalten = true;
+                       plz = i.getId();
+                    }
+                }
+
+                if(plzEnthalten == true){
+                    preiseingabeUnternehmenEntity.setPostleitzahlenId(plz);
+                } else {
+                    PostleitzahlenEntity postleitzahlenEntity = new PostleitzahlenEntity();
+                    postleitzahlenEntity.setPlz(preiseingabeUnternehmen.getPostleitzahl());
+                    plzRepository.save(postleitzahlenEntity);
+                }
+
+/*                preiseingabeUnternehmenEntity.setPostleitzahlenId(i.getId());
+                System.out.println("Richtige Plz gefunden: " +  i.getPlz());
+
+                */
+
+                // speichern in Datenbank PreisEingabeUnternehmenEntity!
+                preiseingabeUnternehmenRepository.save(preiseingabeUnternehmenEntity);
+            }
+
+        }
+
+
+/*      else if (!plzRepository.findAll().contains(i.getPlz())){
+            PostleitzahlenEntity postleitzahlenEntity = new PostleitzahlenEntity();
+            postleitzahlenEntity.setPlz(preiseingabeUnternehmen.getPostleitzahl());
+            plzRepository.save(postleitzahlenEntity);
+        }*/
+
+//        i.getPlz().equals(preiseingabeUnternehmen.getPostleitzahl())
+
+
+
+/*        PreiseingabeUnternehmenEntity preiseingabeUnternehmenEntity = new PreiseingabeUnternehmenEntity();
         UnternehmenEntity unternehmenEntity = new UnternehmenEntity();
 
         preiseingabeUnternehmenEntity.setPreis2700Liter(preiseingabeUnternehmen.getPreis2700Liter());
         preiseingabeUnternehmenEntity.setPreis4850Liter(preiseingabeUnternehmen.getPreis4850Liter());
         preiseingabeUnternehmenEntity.setPreis6400Liter(preiseingabeUnternehmen.getPreis6400Liter());
         preiseingabeUnternehmenEntity.setDatum(new Date(new java.util.Date().getTime()));
-        preiseingabeUnternehmenEntity.setId(unternehmenEntity.getId());
+        preiseingabeUnternehmenEntity.setId(unternehmenEntity.getId());*/
 
 /*
         unternehmenEntity.setAdresse(preiseingabeUnternehmen.getAdresse());
@@ -47,8 +113,8 @@ public class PreiseingabeUnternehmenController {
         unternehmenEntity.setOrt(preiseingabeUnternehmen.getOrt());
         unternehmenEntity.setPlz(preiseingabeUnternehmen.getPlz());
 */
-
+/*
         preiseingabeUnternehmenRepository.save(preiseingabeUnternehmenEntity);
-        unternehemensRepository.save(unternehmenEntity);
+        unternehemensRepository.save(unternehmenEntity);*/
     }
 }
